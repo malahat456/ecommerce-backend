ae81a7f42f5bfe2e5d82d940ec6d4a800ed18816
malahat <batoolmalahat@gmail.com>
2024-11-10 20:39:33 -0800

    e-commerce backend

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');  // Import the User model
 const router = express.Router();
 
